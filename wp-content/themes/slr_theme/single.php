de1274abf5d3b48301e89229b8d97ea50918a2ab
RomaSlobodeniuk <romsl1991@gmail.com>
2016-12-09 17:02:44 +0200

    this is second version

--- wp-content/themes/slr_theme/single.php
+++ wp-content/themes/slr_theme/single.php
@@ -1,20 +1,23 @@
 <?php get_header(); ?>
+<div class="row">
+    <div class="container">
+        <div class="jumbotron col-sm-12>">
+            <div class="col-sm-12>">
 
-<div class="container">
-    <div class="jumbotron>">
+                <main>
+                    <?php while (have_posts()) {
+                        the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+                        <div><?php the_content(); ?></div>
+                        <div class="alert alert-info">
+                            <?php do_action('the_content_from_plugin'); ?>
+                        </div>
+                        <hr/>
+                    <?php } ?>
 
-        <main>
-            <?php while (have_posts()) {
-                the_post(); ?>
-                <h2><?php the_title(); ?></h2>
-                <div><?php the_content(); ?></div>
-                <div class="alert alert-info">
-                    <?php do_action('the_content_from_plugin'); ?>
-                </div>
-                <hr/>
-            <?php } ?>
-
-        </main>
+                </main>
+            </div>
+        </div>
     </div>
 </div>
 
